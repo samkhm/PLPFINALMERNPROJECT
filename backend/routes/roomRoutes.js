5b9eb6cd0ff9c544da76c8ccec9470da982a45b7
Samuel Kimanthi <samuelkimanthi02@gmail.com>
2025-07-15 11:32:58 +0300

    rooms routes

---
+++ backend/routes/roomRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { protect, authorize} = require("../middleware/auth");
+const { createRoom, getAllrooms, getAllBookedRooms, getMyRoom, updateRoom, updateBookedRoom, deleteRooms} = require("../controllers/authRoomController");
+const router = express.Router();
+
+router.post("/", protect, authorize(["admin"]), createRoom);
+router.get("/getAllrooms", protect, authorize(["admin"]), getAllrooms);
+router.get("/getAllbookedRooms", protect, authorize(["admin"]), getAllBookedRooms);
+router.get("/me", protect, getMyRoom);
+router.put("/:id", protect, authorize(["admin"]), updateRoom);
+router.put("/updateBookedRoom/:id", protect, updateBookedRoom);
+router.delete("/:id", protect, authorize(["admin"]), deleteRooms);
