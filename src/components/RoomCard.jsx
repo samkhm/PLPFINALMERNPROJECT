79ea13b32422577a47700731e8bcfbc0b54c1497
Samuel Kimanthi <samuelkimanthi02@gmail.com>
2025-07-16 11:14:56 +0300

    Created room card

---
+++ src/components/RoomCard.jsx
@@ -0,0 +1,37 @@
+import { Card, CardContent, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import { CheckCircleIcon, TrashIcon } from "@heroicons/react";
+import { Button } from "@/components/ui/button";
+import { Label } from "@radix-ui/react-dropdown-menu";
+
+export default function RoomCard(room, onBook, onPay, onDelete){
+    return(
+        <Card className={`relative animation-fade ${
+            room.booked ? "bg-green-300" : ""
+        }`}>
+
+            <CardHeader>
+                <CardTitle className="text-lg font-semibold">
+                 {room.roomNumber}
+                </CardTitle>
+             </CardHeader>
+
+             <CardContent>
+                <p className="text-md font-semibold">Amount: {room.price}</p>
+             </CardContent>
+
+             <CardFooter className="flex justify-between gap-2">
+                <Button className={`bg-red-400 ${room.booked ? "bg-yellow-200" : ""}`}>{room.status}</Button>
+                <Button className={`bg-red-400 ${room.booked ? "bg-yellow-200" : ""}`}>Payment</Button>
+                <Button
+                  size ="icon"
+                  variant = "destructive"
+                  onClick={() => onDelete(room._id)}
+                >
+                    <TrashIcon className="h-5 w-5"/>
+                </Button>
+             </CardFooter>
+
+
+        </Card>
+    )
+}
