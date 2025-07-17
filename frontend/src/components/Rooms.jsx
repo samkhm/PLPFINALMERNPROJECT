ff5765f81c1ffd4043665751869ec6d2dce44ab6
Samuel Kimanthi <samuelkimanthi02@gmail.com>
2025-07-17 12:59:35 +0300

    admin creating room

--- frontend/src/components/Rooms.jsx
+++ frontend/src/components/Rooms.jsx
@@ -1,5 +1,8 @@
 import RoomCard from "./RoomCard";
+import { getUserRole } from "@/utils/auth";
+
 export default function Rooms({rooms, loading, onDelete}){
+    const userRole = getUserRole();
 
          if (loading){
         return(
@@ -14,34 +17,76 @@ export default function Rooms({rooms, loading, onDelete}){
             </>
         );
     }
-    return(
-        <div className="max-w-5xl mx-auto p-4">
-           <section
-               className="grid gap-6
-               sm:grid-cols-2
-               lg:grid-cols-3
-               xl:grid-cols-4"
-            >
-                {rooms.map(r => (
-                    <RoomCard
-                        key={r._id}
-                        room={r}                        
-                        onDelete={onDelete}
-                     />
 
-                ))}
+        if(userRole === 'user'){
 
-            </section>
+    
+                return(
+                    <div className="max-w-5xl mx-auto p-4">
+                    <section
+                        className="grid gap-6
+                        sm:grid-cols-2
+                        lg:grid-cols-3
+                        xl:grid-cols-4"
+                        >
+                            {rooms.map(r => (
+                                <RoomCard
+                                    key={r._id}
+                                    room={r}                        
+                                    onDelete={onDelete}
+                                />
 
-            {rooms.length === 0 &&(
-            <div className="text-centre py-12">
-                <p className="text-gray-500 dark:text-gray-400">
-                    No rooms found
-                </p>
-            </div>
-          )}
+                            ))}
 
-        </div>
-    )
+                        </section>
+
+                        {rooms.length === 0 &&(
+                        <div className="text-centre py-12">
+                            <p className="text-gray-500 dark:text-gray-400">
+                                No rooms found
+                            </p>
+                        </div>
+                    )}
+
+                    </div>
+                );
+            }
+
+        if (userRole === 'admin'){
+            return(
+                //  create a room
+                
+
+
+
+                <div className="max-w-5xl mx-auto p-4">
+                    <section
+                        className="grid gap-6
+                        sm:grid-cols-2
+                        lg:grid-cols-3
+                        xl:grid-cols-4"
+                        >
+                            {rooms.map(r => (
+                                <RoomCard
+                                    key={r._id}
+                                    room={r}                        
+                                    onDelete={onDelete}
+                                />
+
+                            ))}
+
+                        </section>
+
+                        {rooms.length === 0 &&(
+                        <div className="text-centre py-12">
+                            <p className="text-gray-500 dark:text-gray-400">
+                                No rooms found
+                            </p>
+                        </div>
+                    )}
+
+                 </div>
+            )
+        }
 
 }
