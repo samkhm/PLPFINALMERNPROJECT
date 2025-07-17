9520946e1235058e73923671386bdf9f490e72f0
Samuel Kimanthi <samuelkimanthi02@gmail.com>
2025-07-17 10:43:20 +0300

    created main content in dashboard

--- frontend/src/components/MainContent.jsx
+++ frontend/src/components/MainContent.jsx
@@ -1,1 +1,23 @@
-export default function MainContent
+export default function MainContent({ activeSection }){
+    let content;
+    switch(activeSection){
+        case 'home':
+            content = <div>Welcome to dashboard</div>
+            break;
+        case 'rooms':
+            content = <div>book your rooms</div>
+            break;
+        case 'receipts':
+            content = <div>Your receipts</div>
+            break;
+        default:
+            content = <div>Select a section from the side bar</div>
+    }
+
+    return(
+        <div className="p-6 bg-gray-100 w-full">
+            <h1 className="text-xl font-semibold mb-4 capitalize">{activeSection}</h1>
+            <div>{content}</div>
+        </div>
+    );
+};
