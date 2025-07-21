980152781556a9e55387719fc74ab760e201d6b8
Samuel Kimanthi <samuelkimanthi02@gmail.com>
2025-07-21 22:23:38 +0300

    price required

--- backend/models/Rooms.js
+++ backend/models/Rooms.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const roomSchema = new mongoose.Schema({
     roomNumber: { type: String, required: true, unique: true },
-    price: { type: Number, required: true },
+    price: { type: Number, required: true, required: true },
     location: { type: String},
     availability: { type: Boolean, default: true },
     booked: { type: Boolean, default: false },
